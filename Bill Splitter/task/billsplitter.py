c04463dab5d6999f856276cd24818099e9fd3902
drtierney <d.r.tierney@hotmail.co.uk>
2021-10-09 12:12:04 +0100

    Stage #2 completed
    Stage 2/4: The bill has arrived

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -7,5 +7,9 @@ if count < 1:
 
 print('Enter the name of every friend (including you), each on a new line:')
 friends = [input() for _ in range(count)]
-friends_bills = dict.fromkeys(friends, 0)
+
+print("Enter the total bill value:")
+total_bill = int(input())
+split_amount = round(total_bill / len(friends), 2)
+friends_bills = dict.fromkeys(friends, split_amount)
 print(friends_bills)
