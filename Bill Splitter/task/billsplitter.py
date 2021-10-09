f3ae8f50461a042b595ceca92a64f9da14edf9cd
drtierney <d.r.tierney@hotmail.co.uk>
2021-10-09 12:13:09 +0100

    Stage #3 completed
    Stage 3/4: The Lucky One

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,4 +1,6 @@
-# write your code here
+import random
+
+
 print('Enter the number of friends joining (including you):')
 count = int(input())
 if count < 1:
@@ -6,10 +8,18 @@ if count < 1:
     exit()
 
 print('Enter the name of every friend (including you), each on a new line:')
-friends = [input() for _ in range(count)]
+friend_list = [input() for _ in range(count)]
 
-print("Enter the total bill value:")
+print('Enter the total bill value:')
 total_bill = int(input())
-split_amount = round(total_bill / len(friends), 2)
-friends_bills = dict.fromkeys(friends, split_amount)
-print(friends_bills)
+split_amount = round(total_bill / len(friend_list), 2)
+split_bills = dict.fromkeys(friend_list, split_amount)
+
+print('Do you want to use the "Who is lucky?" feature? Write Yes/No')
+confirm = input()
+if confirm != 'Yes':
+    print('No one is going to be lucky')
+    exit()
+
+lucky_one = random.choice(list(split_bills.keys()))
+print(f"{lucky_one} is the lucky one!")
